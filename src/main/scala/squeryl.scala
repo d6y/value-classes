0fedd73a34cd1def93925c36db3af6e21822223a
Richard Dallaway <richard@dallaway.com>
2014-08-29 17:30:33 +0100

    AnyVal with Squeryl.

--- src/main/scala/squeryl.scala
+++ src/main/scala/squeryl.scala
@@ -2,14 +2,38 @@ import org.squeryl._
 import org.squeryl.dsl._
 
 object SquerylEntrypoint extends PrimitiveTypeMode {
+
+  class PlanetPK(val value: Long) extends AnyVal {
+    override def toString = s"PlanetPK($value)"
+  }
+
+  class MoonPK(val value: Long) extends AnyVal {
+    override def toString = s"MoonPK($value)"
+  }
+
+  implicit val planetPKTEF = new NonPrimitiveJdbcMapper[Long, PlanetPK, TLong](longTEF, this) {
+    def convertFromJdbc(v: Long) = new PlanetPK(v)
+    def convertToJdbc(v: PlanetPK) = v.value
+  }
+
+  implicit def planetPKToTE(v: PlanetPK) = planetPKTEF.create(v)
+
+  // copy & paste
+
+  implicit val moonPKTEF = new NonPrimitiveJdbcMapper[Long, MoonPK, TLong](longTEF, this) {
+    def convertFromJdbc(v: Long) = new MoonPK(v)
+    def convertToJdbc(v: MoonPK) = v.value
+  }
+
+  implicit def moonPKToTE(v: MoonPK) = moonPKTEF.create(v)
 }
 
 import SquerylEntrypoint._
 
 object SolarSystem extends Schema {
 
-  case class Planet(id: Long, name: String)
-  case class Moon(id: Long, name: String, planetId: Long)
+  case class Planet(id: PlanetPK, name: String)
+  case class Moon(id: MoonPK, name: String, planetId: PlanetPK)
 
   val planets = table[Planet]("planet")
   val moons   = table[Moon]("moon")
@@ -25,8 +49,21 @@ object SquerylAnyValMain extends App {
     //Session.currentSession.setLogger( println )
 
     println(
-      from(planets)(p => where(p.id === 1) select(p)).headOption
+      from(planets)(p => where(p.id === new PlanetPK(1)) select(p)).headOption
     )
+
+    def findPlanetById(id: PlanetPK) =
+      from(planets)(p => where(p.id === id) select(p)).headOption
+
+    // Won't compile: type mismatch
+    //findPlanetById(new MoonPK(1))
+
+
+    // Argh.  This compiles and runs, which is annoying.
+    println(
+      from(planets)(p => where(p.id === new MoonPK(1)) select(p)).headOption
+    )
+
   }
 }
 
